9366f23087c5701de5586bf9941710409f828f7f
Starman <30315137+Starman3787@users.noreply.github.com>
2021-09-17 22:13:44 +0100

    uart

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include "timers/systick/systick.h"
 #include "drivers/led/led.h"
+#include "drivers/uart/uart.h"
 #include "util/delay/delay.h"
 
 volatile uint64_t ticks = 0;
@@ -11,6 +12,7 @@ void main(void)
     toggle_led();
     while (1)
     {
+        write_uart('a');
         delay_ms(1000);
         toggle_led();
     }
