dd803c7d6ec3b79212865daee1e211aa70ccac36
Starman <30315137+Starman3787@users.noreply.github.com>
2021-09-17 22:13:51 +0100

    uart

--- SystemInit.c
+++ SystemInit.c
@@ -1,12 +1,14 @@
 #include <stdint.h>
 #include "timers/systick/systick.h"
 #include "drivers/led/led.h"
+#include "drivers/uart/uart.h"
 #include "bootloader/bootloader.h"
 #include "SystemInit.h"
 
 void SystemInit(void)
 {
     init_systick();
+    init_uart();
     init_led();
 
     long *bootloaderSource = (long *)&__bootloader_copy;
